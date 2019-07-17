df11cd5dff7eb0230bb49d1e40eea6c03e84ebe2
Arniox <nikkdiehl@gmail.com>
2019-07-17 23:20:44 +1200

    Finished Base game
    - Still need to add lambda
    - Still need to add DynamoDB
    - Still need to test on a phone
    - Still need to add UI
    
    - Updated Jovo
    - Added to language model with back to level select intent and reworked BoxIntent
    - Removed reset intent and skip intent
    
    JS
    - Added userLevelSelected variable and outSideSpeech variable
    - Added inGameSounds array for random shuffeling based on level count
    - Completely finished level selection with dynamically unlocking levels
    - Added game initialisation stateless intent to set up the array that you play on
    - Added entire ingame State with multiple intents including box intent, RankIntent, openedIntent, help intent, back to menu intent, back to level selection intent, exit intent and an unhandled intent.
    - Added WinStatelessFunction stateless intent for the winstate
    - Added resources for failing or correctly guessing sounds
    
    - Added two new functions
    - checkEveryBox which checks if you've won or not
    - shuffle(array) which takes an array and mixes it up.

--- Google Home Version/MemoryGameGoogle/src/app.js
+++ Google Home Version/MemoryGameGoogle/src/app.js
@@ -36,6 +36,8 @@ var numOfTimesPlayed = 0;
 //Users first and second box choice
 var firstBoxChoice = 0;
 var secondBoxChoice = 0;
+var userLevelSelected;
+var outSideSpeech = "";
 
 //players current ingame score
 var playerScore = 0;
@@ -128,6 +130,9 @@ const animals = [
     }
 ];
 
+//Create empty array for the game initialisation
+let inGameSounds = [];
+
 //Current States
 // - start: Beginning state. Only a yes or a no are acceptable for playing either one or two players
 // - gettingNames: The program is looking for a users name. Only a common name is accpetable
@@ -160,7 +165,7 @@ app.setHandler({
             currentState = "gettingNames";
 
             //Ask user for their name
-            this.$speech.addText("<p>I hope your excited, but before we can start, can you please tell me what to call you by?</p>");
+            this.$speech.addText("<p>I hope you're excited, but before we can start, can you please tell me what to call you by?</p>");
             this.$reprompt.addText(Reprompt());
             this.followUpState('GetSingleNameState').ask(this.$speech, this.$reprompt);
         },
@@ -283,17 +288,17 @@ app.setHandler({
         let speech = "";
         //If num of times played equal to 0 then introduce the player
         if(numOfTimesPlayed == 0){
-            speech += "<p>We hope you are ready for our memory game.</p>";
+            speech += "<p>We hope you are ready for our memory game. </p>";
         }
 
         //Else just continue
-        speech += "<p>You have, so far, unlocked:</p>";
+        speech += "<p>You have, so far, unlocked: </p>";
         for(const level of levelsUnlocked){
             if(level.unlocked == true){
                 speech += level.name + ", ";
             }
         }
-        speech += "<p>Which level would you like to play?</p>";
+        speech += "<p>Which level would you like to play? You can also go back to the main menu. </p>";
         this.$speech.addText(speech);
         this.$reprompt.addText(Reprompt());
         this.followUpState('LevelSelectionState').ask(this.$speech, this.$reprompt);
@@ -301,58 +306,219 @@ app.setHandler({
 
     LevelSelectionState: {
         LevelSelectionIntent(){
-            let userLevelSelected = this.$inputs.levelNumber.value;
+            userLevelSelected = this.$inputs.levelNumber.value;
             //Check if the user has selected a non-existant level
             if(!(userLevelSelected > 6 || userLevelSelected < 1)){
                 //Check if the user has selected a locked level
                 if(levelsUnlocked[userLevelSelected-1].unlocked == true){
-                    //Instructions - only needed for new players
-                    if(numberOfTimesLoggedIn < 3){
-                        let speech = "<p>At any point in time during the level; you can say exit to quit the game,"+
-                        " back to go back to level selection or score to check your current score</p>"+
-                        "<p>You will also be asked for two boxes to match up together!</p>";
-                    }   
-                    //Level info
-                    speech += "<p>" + levelsUnlocked[userLevelSelected-1].name + " has " +
-                    levelsUnlocked[userLevelSelected-1].numberOfSounds + " sounds to discover. Meaning a total of " +
-                    (levelsUnlocked[userLevelSelected-1].numberOfSounds*2) + " pairs to match up!</p>";                 
-                    //Query
-                    speech += "<p>Ok, are you ready to play " + levelsUnlocked[userLevelSelected-1].name + "?</p>";
-                    this.$speech.addText(speech);
-                    this.$reprompt.addText(Reprompt());
-                    this.tell(this.$speech);
-                    //Create boxes with animals in them
-                    
+                    //Go to initialisation stateless function
+                    return this.toStatelessIntent('InitialisationIntent');
+
                 }else{
-                    this.$speech.addText("<p>Sorry this level needs to be unlocked by playing the prior levels in order.</p>" + Reprompt());
+                    this.$speech.addText("<p>Sorry this level needs to be unlocked by playing the prior levels in order. </p>" + Reprompt());
                     this.$reprompt.addText(Reprompt());
                     this.followUpState('LevelSelectionState').ask(this.$speech, this.$reprompt);
                 }
             }else{
-                this.$speech.addText("<p>Sorry this level does not exist.</p>" + Reprompt());
+                this.$speech.addText("<p>Sorry this level does not exist. </p>" + Reprompt());
                 this.$reprompt.addText(Reprompt());
                 this.followUpState('LevelSelectionState').ask(this.$speech, this.$reprompt);
             }
         },
+        BackToMenuIntent() {
+            //Go back to main menu
+            return this.toStatelessIntent('GiveMenu');
+        },
         Unhandled(){
             //Try again
-            this.$speech.addText("<p>Sorry, I could not understand you.</p>" + Reprompt());
+            this.$speech.addText("<p>Sorry, I could not understand you. </p>" + Reprompt());
             this.$reprompt.addText(Reprompt());
 
             this.followUpState('LevelSelectionState').ask(this.$speech, this.$reprompt);
         },
     },
-    
+
+    //Stateless initialisation function
+    InitialisationIntent(){
+        //Initialise things first
+        //Set state
+        currentState = "inGame";
+        //Set up random boxes
+        let animalNoises = levelsUnlocked[userLevelSelected-1].numberOfSounds;
+        shuffle(animals); //shuffle animal array to pick from
+        //Reset array
+        inGameSounds = [];
+        //Fill array
+        for(let i=0; i<animalNoises; i++){
+            //Add in number of animal noises
+            inGameSounds.push({
+                name: animals[i].name,
+                resource: animals[i].resource,
+                opened: false
+            });
+            //Double animal noises
+            inGameSounds.push({
+                name: animals[i].name,
+                resource: animals[i].resource,
+                opened: false
+            });
+        }
+        //Shuffle new const
+        shuffle(inGameSounds);
+
+        //Instructions - only needed for new players
+        let speech = "";
+        if(numberOfTimesLoggedIn < 3){
+            speech += "<p>At any point in time during the level; you can say exit to quit the game,"+
+            " back to go back to level selection, help to ask for help, "+
+            "check opened to see any opened boxes or score to check your current score! </p>"+
+            "<p>You will also be asked for two boxes to match up together! </p>";
+        }
+        //Level info
+        speech += "<p>" + levelsUnlocked[userLevelSelected-1].name + " has " +
+        (levelsUnlocked[userLevelSelected-1].numberOfSounds*2) + " sounds to discover. Meaning a total of " +
+        levelsUnlocked[userLevelSelected-1].numberOfSounds + " pairs to match up! </p>";
+        //Query
+        speech += "<p>Ok please choose two boxes to start off between 1 and "+inGameSounds.length;
+        //Increase num of times played and reset score
+        numOfTimesPlayed++;
+        //Reset current player score
+        playerScore = 0;
+        this.$speech.addText(speech);
+        this.$reprompt.addText(Reprompt());
+        this.followUpState('InGameState').ask(this.$speech, this.$reprompt);
+    },
 
     InGameState: {
         BoxIntent()
         {
-            currentState = "inGame";            
-
             //Assign animal objects to boxes and then randomize them
             let speech = "";
+            let indexSelected1 = this.$inputs.boxNumberONE.value-1;
+            let indexSelected2 = this.$inputs.boxNumberTWO.value-1;
 
-            speech = "lets begin! Please select a box from 1 to" + (levelsUnlocked[userLevelSelected-1].numberOfSounds*2);
+            //Check if boxes exist
+            if(!(indexSelected1+1 > inGameSounds.length || indexSelected2+1 > inGameSounds.length)){
+                //Check that the user didn't choose the same boxes
+                if(!(indexSelected1 == indexSelected2)){
+                    //Check if either box picked is already opened
+                    if(inGameSounds[indexSelected1].opened == true && inGameSounds[indexSelected2].opened == true){
+                        speech = "<p>both boxes have already opened. </p>";
+
+                    }else if(inGameSounds[indexSelected1].opened == true){
+                        speech = "<p>box "+(indexSelected1+1)+
+                                  " is already opened. </p>";
+                    }else if(inGameSounds[indexSelected2].opened == true){
+                        speech = "<p>box "+(indexSelected2+1)+
+                                  " is already opened. </p>";
+                    }else{
+                        //Play animal sounds
+                        speech += "<p>"+inGameSounds[indexSelected1].resource+" </p>"+
+                                  "<p>"+inGameSounds[indexSelected2].resource+" </p>";
+
+                        //Check now if they match or not
+                        if(inGameSounds[indexSelected1].name == inGameSounds[indexSelected2].name){
+                            //Tell user they where correct
+                            speech += "<p>NICE <audio src='https://s3.amazonaws.com/alexa-hackathon-memory-game-assets/sounds/success.mp3'/>"+
+                                      " Good work! you found a pair of "+inGameSounds[indexSelected1].name+"s! </p>";
+                            //Set now opened boxes to true
+                            inGameSounds[indexSelected1].opened = true;
+                            inGameSounds[indexSelected2].opened = true;
+
+                            //Increase score
+                            playerScore++;
+
+                            //Check if every box has now finished
+                            if(checkEveryBox() == inGameSounds.length){
+                                //Save last sounds to play in stateless intent
+                                outSideSpeech = "<p>"+inGameSounds[indexSelected1].resource+" </p>"+
+                                                "<p>"+inGameSounds[indexSelected2].resource+" </p>";
+                                //Go to win state
+                                return this.toStatelessIntent('WinStatelessFunction');
+                            }
+                        }else{
+                            //Tell user they where incorrect
+                            speech += "<p>WRONG <audio src='https://s3.amazonaws.com/alexa-hackathon-memory-game-assets/sounds/fail2.mp3'/>"+
+                                      " animals from two boxes were not same. </p>";
+                            //Decrement score
+                            playerScore--;
+                        }
+                    }
+                }else{
+                    speech = "<p>Sorry, your two choices are the same. No cheating! </p>";
+                }
+            }else{
+                speech = "<p>One of your box choices does not exist. </p>";
+            }
+
+            speech += "<p>Ok please choose two boxes to continue between 1 and "+inGameSounds.length+" </p>";
+            //Send out prompt
+            this.$speech.addText(speech);
+            this.$reprompt.addText(Reprompt());
+            this.followUpState('InGameState').ask(this.$speech, this.$reprompt);
+        },
+        RankIntent(){
+            let speech = "";
+            speech += "<p>Your highest score so far in this game is: "+playerScore+" </p>";
+
+            //Reprompt the user
+            speech += "<p>Ok please choose two boxes to continue between 1 and "+inGameSounds.length;
+            this.$speech.addText(speech);
+            this.$reprompt.addText(Reprompt());
+            this.followUpState('InGameState').ask(this.$speech, this.$reprompt);
+        },
+        OpenedIntent(){
+            let speech = "";
+            speech += "<p>You have opened: </p>";
+            //Check which boxes where opened
+            let ifNoneOpened = true;
+            for(let i = 0; i < inGameSounds.length; i++){
+                if(inGameSounds[i].opened == true){
+                    ifNoneOpened = false;
+                    speech += "<p>box number "+(i+1)+". It was a "+inGameSounds[i].name+". </p>";
+                    //Add middle text
+                    if(!(i%2==0)){
+                        speech += "<p>and you've opened </p>";
+                    }
+                }
+            }
+            //Check if no boxes are opened
+            if(ifNoneOpened == true){
+                speech += "<p>No boxes have been opened yet! </p>";
+            }
+
+            //Reprompt the user
+            speech += "<p>Ok please choose two boxes to continue between 1 and "+inGameSounds.length;
+            this.$speech.addText(speech);
+            this.$reprompt.addText(Reprompt());
+            this.followUpState('InGameState').ask(this.$speech, this.$reprompt);
+        },
+        HelpIntent(){
+            //Reprompt the user and let them know how to use the app whilst playing
+            let speech = "";
+            speech += "<p>At any point in time during the level; you can say exit to quit the game,"+
+                      " back to go back to level selection, help to ask for help or score to check your current score! </p>";
+            speech += "<p>You will also be asked for two boxes to match up together! </p>";
+            speech += "<p>Ok please choose two boxes to continue between 1 and "+inGameSounds.length;
+
+            this.$speech.addText(speech);
+            this.$reprompt.addText(Reprompt());
+            this.followUpState('InGameState').ask(this.$speech, this.$reprompt);
+        },
+        BackToMenuIntent() {
+            //Go back to main menu
+            return this.toStatelessIntent('GiveMenu');
+        },
+        BackToLevelSelectionIntent(){
+            //Go back to level selection
+            return this.toStatelessIntent('PlayIntentMenuGiver');
+        },
+        ExitIntent(){
+            //Current state is the main menu
+            currentState = "exitGame";
+            //Set player response
+            this.$speech.addText("Thank you for playing our animal memory game, good bye!");
+            this.tell(this.$speech);
         },
         Unhandled(){
             //Try again
@@ -363,13 +529,50 @@ app.setHandler({
         },
     },
 
+    //Win stateless function
+    WinStatelessFunction(){
+        //Set state
+        currentState = "winState";
+
+        //Set final winning text
+        let speech = "";
+        speech += outSideSpeech;
+        speech += "<p>Congradulations on winning "+levelsUnlocked[userLevelSelected-1].name+". </p>";
+
+        //Unlock next level
+        if(!(userLevelSelected > levelsUnlocked.length-1)){
+            levelsUnlocked[userLevelSelected].unlocked = true;
+            speech += "<p>You've now unlocked "+levelsUnlocked[userLevelSelected].name+". </p>";
+        }else{
+            speech += "<p>You have reached the max level for the animal sound pack. </p>";
+        }
+        //If current score is better than highest score than save
+        if(playerScore > bestScore){
+            bestScore = playerScore;
+            //Tell player
+            speech += "<p>You've achieved a new highest score of "+bestScore+". Congratulations!!!!!!!! </p>";
+        }
+
+        speech += "<p>Say level selection to go play your newly unlocked level or say back to go back to the main menu. </p>";
+
+        //Prompt
+        this.$speech.addText(speech);
+        this.$reprompt.addText(Reprompt());
+
+        this.followUpState('BackToMainMenuState').ask(this.$speech, this.$reprompt);
+    },
+
     BackToMainMenuState: {
         BackToMenuIntent() {
             return this.toStatelessIntent('GiveMenu');
         },
+        BackToLevelSelectionIntent(){
+            //Go back to level selection
+            return this.toStatelessIntent('PlayIntentMenuGiver');
+        },
         Unhandled(){
             //Try again
-            this.$speech.addText("<p>Sorry, I could not understand you.</p>" + Reprompt());
+            this.$speech.addText("<p>Sorry, I could not understand you. </p>" + Reprompt());
             this.$reprompt.addText(Reprompt());
 
             this.followUpState('BackToMainMenuState').ask(this.$speech, this.$reprompt);
@@ -383,6 +586,31 @@ module.exports.app = app;
 // ------------------------------------------------------------------
 // FUNCTIONS
 // ------------------------------------------------------------------
+//Check if every box has been Opened
+function checkEveryBox(){
+    //Go through all boxes and check how many are opened
+    let countedOpenedBoxes = 0;
+    for(let p=0; p<inGameSounds.length; p++){
+        if(inGameSounds[p].opened == true){
+            countedOpenedBoxes++;
+        }
+    }
+
+    //Return count of opened boxes
+    return countedOpenedBoxes;
+}
+
+//shuffeling function
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i+1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+//Dynamic reprompt function
 function Reprompt(){
     let text = "";
     if(currentState === "start"){
@@ -396,9 +624,11 @@ function Reprompt(){
     }else if(currentState === "soundSelect"){
         text = "Please choose a sound package to play!, the available sound packages are: <list packages>";
     }else if(currentState === "levelSelect"){
-        text = "Please choose a level to play!, the levels available are: <list available levels>";
+        text = "Please choose a level to play or go back to the main menu";
     }else if(currentState === "inGame"){
-        text = "Please choose a box and then another one to match it, the unopen boxes are: <list unopened boxes>";
+        text = "Please choose a box and then another one to match it, or you can choose to either exit, back to menu, back to level select, help, check your score or check opened boxes";
+    }else if(currentState === "winState"){
+        text = "Please choose to either go back to the level selection menu or the main menu";
     }else if(currentState === "playingGame"){
         text = "Please choose a level to select and play";
     }
